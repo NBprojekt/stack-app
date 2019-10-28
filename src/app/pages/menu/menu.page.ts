360acf6f344f1335cdab624e9f1ce7eece648910
Norbert Bartko <mail@norbert-bartko.de>
2019-10-28 13:32:08 +0100

    Add basic routing logic

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, RouterEvent, NavigationEnd } from '@angular/router';
+
+import { filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage implements OnInit {
+  public selectedUrl: string;
 
-  constructor() { }
+  public pages = [
+    {
+        title: 'Home',
+        url: './tabs/home',
+        icon: 'home',
+    },
+    {
+        title: 'Search',
+        url: './tabs/search',
+        icon: 'search',
+    },
+  ];
 
-  ngOnInit() {
+  constructor(
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.selectedUrl = '';
+
+    this.router.events.pipe(
+        filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+      ).subscribe((event: NavigationEnd) => {
+        this.selectedUrl = event.url;
+      });
   }
-
 }
