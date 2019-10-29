6f2bda83341e2d305e0f82e2702e2a46287bf166
Norbert Bartko <mail@norbert-bartko.de>
2019-10-29 15:35:15 +0100

    Add login page to app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'menu', pathMatch: 'full' },
   { path: 'menu', loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule) },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule) },
 ];
 
 @NgModule({
