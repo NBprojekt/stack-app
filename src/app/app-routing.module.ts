9ba4845b61d3fb1f2132daec064a8d3065542a82
Norbert Bartko <mail@norbert-bartko.de>
2019-10-30 18:06:15 +0100

    Add routing guard for menu module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './guards/auth/auth.guard';
+
 const routes: Routes = [
   { path: '', redirectTo: 'menu', pathMatch: 'full' },
-  { path: 'menu', loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule) },
+  { path: 'menu', loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule), canActivate: [AuthGuard], canActivateChild: [AuthGuard] },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule) },
 ];
 
