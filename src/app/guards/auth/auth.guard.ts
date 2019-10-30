0caed411d6f072823dde8d85fadd6420408631a5
Norbert Bartko <mail@norbert-bartko.de>
2019-10-30 18:02:58 +0100

    Add auth service to auth guard

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+
 import { Observable } from 'rxjs';
+
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authService.isAuthenticated() ? true : this.router.parseUrl('/login');
   }
   canActivateChild(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authService.isAuthenticated() ? true : this.router.parseUrl('/login');
   }
-  
 }
