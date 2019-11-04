38995acab50029266a7d6bbbda58d0b05c291aaa
Norbert Bartko <mail@norbert-bartko.de>
2019-11-04 11:58:54 +0100

    Add dynamic header title

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { PopoverController } from '@ionic/angular';
 import { MoreComponent } from './more/more.component';
+import { filter } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,19 +11,42 @@ import { MoreComponent } from './more/more.component';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  public title: string;
+  public inbox: number;
+  public achievements: number;
+  public reviwQueues: boolean;
 
   constructor(
+    private router: Router,
     private popoverController: PopoverController,
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      ).subscribe((event: NavigationEnd) => {
+        const url = event.url;
+        console.log(url)
+        this.title = this.firstToUpper(url.split('/').pop());
+      });
 
-  public async showMore(ev: any) {
+    this.inbox = 5;
+    this.achievements = 120;
+    this.reviwQueues = true;
+  }
+
+  public async showMore(event: any) {
     const popover = await this.popoverController.create({
       component: MoreComponent,
-      event: ev,
-      translucent: true
+      translucent: true,
+      event,
     });
     return await popover.present();
+  }
+
+  private firstToUpper(s: string): string {
+    const first = s[0].toUpperCase();
+    return first + s.slice(1, s.length);
   }
 }
