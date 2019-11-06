6f8a174050b37b25d182d783137e077662be8e1e
Norbert Bartko <mail@norbert-bartko.de>
2019-11-06 11:26:41 +0100

    Add refresher

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  public backdrop = false;
+  public questions: Array<any>;
 
   constructor() { }
 
   ngOnInit() {
+    this.doRefresh();
   }
 
+  doRefresh(event?): void {
+
+    setTimeout(() => {
+      this.questions = [1, 2, 3, 4];
+      if (event) {
+        event.target.complete();
+      }
+    }, 1000);
+  }
 }
