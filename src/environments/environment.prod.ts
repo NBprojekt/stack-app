237ebb6352b287a38ab4acc76f9b70669cce0dd9
Norbert Bartko <mail@norbert-bartko.de>
2019-11-07 18:25:56 +0100

    Add api url and version to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
+  api: {
+    url: 'https://api.stackexchange.com/',
+    version: '2.2/',
+  },
   oAuth: {
     clientId: 16531,
     redirectUrl: 'http://localhost/',
