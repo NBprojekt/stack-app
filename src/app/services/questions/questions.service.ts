a3a08c12404f326b59e6d018e1477681f9ba4fc0
Norbert Bartko <mail@norbert-bartko.de>
2019-11-07 19:03:32 +0100

    Create getQuestion function

--- src/app/services/questions/questions.service.ts
+++ src/app/services/questions/questions.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { IQuestionPreview } from 'src/app/interfaces/question-preview';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionsService {
+  private readonly url = environment.api.url + environment.api.version;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public getQuestions(site?: string, order?: string, sort?: string): Observable<Array<IQuestionPreview>> {
+    const headers = new HttpHeaders()
+      .set('Accept', '*/*');
+
+    const params = new HttpParams()
+      .set('site', site || 'stackoverflow')
+      .set('order', order || 'desc')
+      .set('sort', sort || 'activity');
+
+    return this.http.get<Array<IQuestionPreview>>(`${this.url}questions`, {headers, params});
+  }
 }
