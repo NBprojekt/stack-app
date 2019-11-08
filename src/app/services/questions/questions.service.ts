2beeb9b6a19c6d310533c1cfa72d56390323d6c2
Norbert Bartko <mail@norbert-bartko.de>
2019-11-08 17:21:48 +0100

    Add key and page params for question calls

--- src/app/services/questions/questions.service.ts
+++ src/app/services/questions/questions.service.ts
@@ -8,22 +8,28 @@ import { environment } from 'src/environments/environment';
 import { IQuestion } from 'src/app/interfaces/question';
 import { IQuestionFilter } from 'src/app/interfaces/question-filter';
 import { IResponse } from 'src/app/interfaces/response';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionsService {
   private readonly url = environment.api.url + environment.api.version;
+  private readonly pagesize = 25;
 
   constructor(
     private http: HttpClient,
+    private authService: AuthService,
   ) { }
 
-  public getQuestions(filter?: IQuestionFilter): Observable<IResponse> {
+  public getQuestions(filter?: IQuestionFilter, page?: number): Observable<IResponse> {
     const headers = new HttpHeaders()
       .set('Accept', '*/*');
 
     const params = new HttpParams()
+      .set('key', this.authService.getToken())
+      .set('page', page ? page.toString() : '1')
+      .set('pagesize', this.pagesize.toString())
       .set('site', filter && filter.site || 'stackoverflow')
       .set('order', filter && filter.order || 'desc')
       .set('sort', filter && filter.sort || 'activity');
@@ -36,6 +42,7 @@ export class QuestionsService {
       .set('Accept', '*/*');
 
     const params = new HttpParams()
+      .set('key', this.authService.getToken())
       .set('site', site || 'stackoverflow')
       .set('filter', 'withbody');
 
