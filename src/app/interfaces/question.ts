6a52d003e0b59268f0ac4b637a143822b81b9c98
Norbert Bartko <mail@norbert-bartko.de>
2019-11-12 15:23:14 +0100

    Fix typo

--- src/app/interfaces/question.ts
+++ src/app/interfaces/question.ts
@@ -1,7 +1,7 @@
 import { SafeHtml } from '@angular/platform-browser';
 
 export interface IQuestion {
-  anwser_count: number;
+  answer_count: number;
   creation_date: number;
   is_answered: boolean;
   last_activity_date: number;
@@ -12,6 +12,7 @@ export interface IQuestion {
   tags: Array<string>;
   title: string;
   view_count: number;
+  accepted_answer_id?: number;
   closed_date?: number;
   closed_reason?: string;
   last_edit_date?: number;
