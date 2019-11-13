7181ec79070bd0e798c7a1424e66c37f52faada3
Norbert Bartko <mail@norbert-bartko.de>
2019-11-13 13:58:15 +0100

    Implement voting in question

--- src/app/pages/question/question.module.ts
+++ src/app/pages/question/question.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { QuestionPage } from './question.page';
 import { CommonPipesModule } from 'src/app/pipes/common-pipes.module';
+import { VotingModule } from 'src/app/components/voting/voting.module';
 
 const routes: Routes = [
   {
@@ -22,6 +23,7 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes),
     CommonPipesModule,
+    VotingModule
   ],
   declarations: [
     QuestionPage,
