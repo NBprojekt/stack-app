4dc0de1fdaf673c7ed5d61747e445adbc79c357e
Norbert Bartko <mail@norbert-bartko.de>
2019-11-15 17:25:46 +0100

    Fix calculation for shorten number pipe

--- src/app/pipes/shorten-number/shorten-number.pipe.ts
+++ src/app/pipes/shorten-number/shorten-number.pipe.ts
@@ -12,14 +12,14 @@ export class ShortenNumberPipe implements PipeTransform {
 
     switch (x.length) {
       case 4: return `${x[0]}.${x[1]}k`;
-      case 5: return `${x.slice(0, 1)}.${x[2]}k`;
-      case 6: return `${x.slice(0, 2)}k`;
+      case 5: return `${x.slice(0, 2)}.${x[2]}k`;
+      case 6: return `${x.slice(0, 3)}k`;
       case 7: return `${x[0]}.${x[1]}m`;
-      case 8: return `${x.slice(0, 1)}.${x[2]}m`;
-      case 9: return `${x.slice(0, 2)}m`;
+      case 8: return `${x.slice(0, 2)}.${x[2]}m`;
+      case 9: return `${x.slice(0, 3)}m`;
       case 10: return `${x[0]}.${x[1]}m`;
-      case 11: return `${x.slice(0, 1)}.${x[2]}m`;
-      case 12: return `${x.slice(0, 2)}m`;
+      case 11: return `${x.slice(0, 2)}.${x[2]}m`;
+      case 12: return `${x.slice(0, 3)}m`;
 
       default: return x;
     }
