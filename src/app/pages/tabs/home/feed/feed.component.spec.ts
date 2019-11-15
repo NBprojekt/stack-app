909588a488c89fb5de8d8e1e59a28d260723910e
Norbert Bartko <mail@norbert-bartko.de>
2019-11-15 18:31:07 +0100

    Add test to feed component

--- src/app/pages/tabs/home/feed/feed.component.spec.ts
+++ src/app/pages/tabs/home/feed/feed.component.spec.ts
@@ -7,6 +7,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { IQuestion } from 'src/app/interfaces/question';
 
 describe('FeedComponent', () => {
   let component: FeedComponent;
@@ -37,5 +38,32 @@ describe('FeedComponent', () => {
 
   it('Should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should init', () => {
+    component.ngOnInit();
+
+    expect(component.filter).toBe('interesting');
+  });
+
+  it('Should create a number array', () => {
+    const array = component.fill(10);
+
+    expect(array.length).toEqual(10);
+    expect(typeof array[0]).toBe('number');
+    expect(array[0]).toEqual(1);
+  });
+
+  it('Should mark question as hot', () => {
+    const hotQuestion = {
+      view_count: 5,
+      answer_count: 0,
+      score: 0,
+      answers: [{score: 0}],
+      creation_date: 1573838839,
+      last_activity_date: 1573838839,
+    } as IQuestion;
+
+    expect(component.questionIsHot(hotQuestion)).toBeTruthy();
   });
 });
