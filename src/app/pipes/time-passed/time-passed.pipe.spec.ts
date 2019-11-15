375ae213f6f8341d2e9e9c1a86c271184169cbbe
Norbert Bartko <mail@norbert-bartko.de>
2019-11-15 17:07:34 +0100

    Create tests for time passed pipe

--- src/app/pipes/time-passed/time-passed.pipe.spec.ts
+++ src/app/pipes/time-passed/time-passed.pipe.spec.ts
@@ -1,8 +1,63 @@
 import { TimePassedPipe } from './time-passed.pipe';
 
+import * as moment from 'moment';
+
 describe('TimePassedPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimePassedPipe();
+  let pipe: TimePassedPipe;
+
+  beforeEach(() => {
+    pipe = new TimePassedPipe();
+  });
+
+  it('Should create', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  describe('Should format', () => {
+    it('seconds ago', () => {
+      const unixTimestamp = moment().unix();
+      expect(pipe.transform(unixTimestamp)).toContain('secs ago');
+    });
+
+    it('minutes ago', () => {
+      const unixTimestamp = moment().subtract(5, 'minutes').unix();
+      expect(pipe.transform(unixTimestamp)).toContain('min ago');
+    });
+
+    it('hours ago', () => {
+      const unixTimestamp = moment().subtract(2, 'hours').unix();
+      expect(pipe.transform(unixTimestamp)).toContain('hours ago');
+    });
+
+    it('today', () => {
+      const unixTimestamp = moment().subtract(12, 'hours').unix();
+      expect(pipe.transform(unixTimestamp)).toContain('today');
+    });
+
+    it('yesterday', () => {
+      const unixTimestamp = moment().subtract(1, 'days').unix();
+      expect(pipe.transform(unixTimestamp)).toContain('yesterday');
+    });
+
+    it('days ago', () => {
+      const unixTimestamp = moment().subtract(3, 'days').unix();
+      expect(pipe.transform(unixTimestamp)).toContain('days ago');
+    });
+
+    it('weeks ago', () => {
+      const unixTimestamp = moment().subtract(3, 'weeks').unix();
+      expect(pipe.transform(unixTimestamp)).toContain('weeks ago');
+    });
+
+    it('months ago', () => {
+      const unixTimestamp = moment().subtract(5, 'months').unix();
+      expect(pipe.transform(unixTimestamp)).toContain('months ago');
+    });
+
+    it('years ago', () => {
+      const unixTimestamp = moment().subtract(5, 'years').unix();
+      expect(pipe.transform(unixTimestamp)).toContain('years,');
+      expect(pipe.transform(unixTimestamp)).toContain('months ago');
+    });
   });
 });
