b7d4ca1e598bbcf8bd184d314603eb1b360f7154
Norbert Bartko <mail@norbert-bartko.de>
2019-11-15 17:26:16 +0100

    Create test for shorten number pipe

--- src/app/pipes/shorten-number/shorten-number.pipe.spec.ts
+++ src/app/pipes/shorten-number/shorten-number.pipe.spec.ts
@@ -1,8 +1,39 @@
 import { ShortenNumberPipe } from './shorten-number.pipe';
 
 describe('ShortenNumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShortenNumberPipe();
+  let pipe: ShortenNumberPipe;
+
+  beforeEach(() => {
+    pipe = new ShortenNumberPipe();
+  });
+
+  it('Should create', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  describe('Should format', () => {
+    it('up to hundreds', () => {
+      expect(pipe.transform(297)).toEqual('297');
+    });
+
+    it('thousands', () => {
+      expect(pipe.transform(5297)).toEqual('5.2k');
+    });
+    it('ten thousands', () => {
+      expect(pipe.transform(75297)).toEqual('75.2k');
+    });
+    it('hundred thousands', () => {
+      expect(pipe.transform(175297)).toEqual('175k');
+    });
+
+    it('millions', () => {
+      expect(pipe.transform(8175297)).toEqual('8.1m');
+    });
+    it('ten millions', () => {
+      expect(pipe.transform(38175297)).toEqual('38.1m');
+    });
+    it('hundred millions', () => {
+      expect(pipe.transform(438175297)).toEqual('438m');
+    });
   });
 });
