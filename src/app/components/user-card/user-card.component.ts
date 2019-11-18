c537b5788fbc3298b2c7e083049f09a9ddf99a92
Norbert Bartko <mail@norbert-bartko.de>
2019-11-18 12:04:28 +0100

    Remove unused functions

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,22 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IUser } from 'src/app/interfaces/user';
-import { IQuestion } from 'src/app/interfaces/question';
-import { IAnswer } from 'src/app/interfaces/answer';
 
 @Component({
   selector: 'user-card',
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.scss'],
 })
-export class UserCardComponent implements OnInit {
+export class UserCardComponent {
   @Input() user: IUser;
 
   @Input() action?: string;
   @Input() timestamp?: number;
   @Input() highlight?: boolean;
-
-  constructor() { }
-
-  ngOnInit() {}
-
 }
