5bd91fa009d0d76459dc0c04d8dcc0d6f560b3b7
Norbert Bartko <mail@norbert-bartko.de>
2019-11-19 12:42:08 +0100

    Create user card skeleton

---
+++ src/app/components/user-card/user-card-skeleton/user-card-skeleton.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'user-card-skeleton',
+  template: `<ion-skeleton-text animated class="user-card-skeleton"></ion-skeleton-text>`,
+  styleUrls: ['./user-card-skeleton.component.scss'],
+})
+export class UserCardSkeletonComponent {}
