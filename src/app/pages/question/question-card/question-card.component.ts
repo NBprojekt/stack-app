e4d136015cac1fd0cbee4bcfede121a3b1e3c559
Norbert Bartko <mail@norbert-bartko.de>
2019-11-19 14:02:03 +0100

    Move question card in its own question card component

---
+++ src/app/pages/question/question-card/question-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { IQuestion } from 'src/app/interfaces/question';
+
+@Component({
+  selector: 'question-card',
+  templateUrl: './question-card.component.html',
+  styleUrls: ['./question-card.component.scss'],
+})
+export class QuestionCardComponent {
+  @Input() question: IQuestion;
+}
