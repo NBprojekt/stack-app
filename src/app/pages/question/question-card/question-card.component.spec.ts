e10387c6a24151fab4ef0b4c07f4516fde48fe82
Norbert Bartko <mail@norbert-bartko.de>
2019-11-19 15:54:15 +0100

    Fix import issues for questin card testing

--- src/app/pages/question/question-card/question-card.component.spec.ts
+++ src/app/pages/question/question-card/question-card.component.spec.ts
@@ -1,7 +1,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { CommonPipesModule } from 'src/app/pipes/common-pipes.module';
 
 import { QuestionCardComponent } from './question-card.component';
+import { VotingModule } from 'src/app/components/voting/voting.module';
+import { CommentModule } from 'src/app/components/comment/comment.module';
+import { UserCardModule } from 'src/app/components/user-card/user-card.module';
 
 describe('QuestionCardComponent', () => {
   let component: QuestionCardComponent;
@@ -10,6 +16,13 @@ describe('QuestionCardComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ QuestionCardComponent ],
+      imports: [
+        IonicModule,
+        CommonPipesModule,
+        VotingModule,
+        UserCardModule,
+        CommentModule,
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
