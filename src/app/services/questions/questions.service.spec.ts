b01a52d212996c290dbedc1cd00776f268be8f67
Norbert Bartko <mail@norbert-bartko.de>
2019-11-19 10:13:40 +0100

    Move hot question calculation to question service

--- src/app/services/questions/questions.service.spec.ts
+++ src/app/services/questions/questions.service.spec.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { RouterTestingModule } from '@angular/router/testing';
+import { IQuestion } from 'src/app/interfaces/question';
 
 describe('QuestionsService', () => {
 
@@ -24,5 +25,18 @@ describe('QuestionsService', () => {
   it('Should create', () => {
     const service: QuestionsService = TestBed.get(QuestionsService);
     expect(service).toBeTruthy();
+  });
+
+  it('Should mark question as hot', () => {
+    const hotQuestion = {
+      view_count: 5,
+      answer_count: 0,
+      score: 0,
+      answers: [{score: 0}],
+      creation_date: 1573838839,
+      last_activity_date: 1573838839,
+    } as IQuestion;
+
+    expect(TestBed.get(QuestionsService).questionIsHot(hotQuestion)).toBeTruthy();
   });
 });
