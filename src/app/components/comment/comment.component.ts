a64165445c17081f48bbb5f211649a4a87aa5cd4
Norbert Bartko <mail@norbert-bartko.de>
2019-11-19 14:45:31 +0100

    Create comment component

---
+++ src/app/components/comment/comment.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'comments',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.scss'],
+})
+export class CommentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/components/comment/comment.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+
+import { CommonPipesModule } from 'src/app/pipes/common-pipes.module';
+
+import { CommentComponent } from './comment.component';
+
+@NgModule({
+  declarations: [
+    CommentComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    CommonPipesModule,
+  ],
+  exports: [
+    CommentComponent,
+  ]
+})
+export class CommentModule { }
