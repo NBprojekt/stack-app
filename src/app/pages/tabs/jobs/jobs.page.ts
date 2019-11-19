5b736717365e79fcd41dc4cc008cf39b87255909
Norbert Bartko <mail@norbert-bartko.de>
2019-11-19 17:12:56 +0100

    Clean up

--- src/app/pages/tabs/jobs/jobs.page.ts
+++ src/app/pages/tabs/jobs/jobs.page.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.page.html',
   styleUrls: ['./jobs.page.scss'],
 })
-export class JobsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class JobsPage {}
