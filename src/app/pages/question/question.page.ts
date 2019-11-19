6405c82a25afe8019ac747c1bd3d90774befe83f
Norbert Bartko <mail@norbert-bartko.de>
2019-11-19 15:54:49 +0100

    Map question comments to save html

--- src/app/pages/question/question.page.ts
+++ src/app/pages/question/question.page.ts
@@ -34,6 +34,7 @@ export class QuestionPage implements OnInit {
     this.questionsService.getQuestion(id).subscribe((response: IResponse) => {
       this.question = response.items[0] as IQuestion;
       this.question.body = this.sanitizer.bypassSecurityTrustHtml(this.question.body as string);
+      this.question.comments.map(comment => comment.body = this.sanitizer.bypassSecurityTrustHtml(comment.body as string));
     });
   }
 
