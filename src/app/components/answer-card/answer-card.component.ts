eda94e666152b22b955f3f086463c34fe64e6e62
Norbert Bartko <mail@norbert-bartko.de>
2019-11-20 18:56:14 +0100

    Place answers in their own component

--- src/app/components/answer-card/answer-card.component.ts
+++ src/app/components/answer-card/answer-card.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { IAnswer } from 'src/app/interfaces/answer';
 
 @Component({
   selector: 'answer-card',
   templateUrl: './answer-card.component.html',
   styleUrls: ['./answer-card.component.scss'],
 })
-export class AnswerCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class AnswerCardComponent {
+  @Input() answer: IAnswer;
 }
