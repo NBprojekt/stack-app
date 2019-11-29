382386fd35be5cddb3dc81d7fdeaefd333939d07
Norbert Bartko <mail@norbert-bartko.de>
2019-11-29 18:13:03 +0100

    Adjust promise return type

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -23,7 +23,7 @@ export class LoginPage implements OnInit, OnDestroy {
     private authService: AuthService,
   ) { }
 
-  async ngOnInit(): Promise<any> {
+  async ngOnInit(): Promise<void> {
     this.slidesLength = await this.slides.length();
 
     this.slides.ionSlidePrevEnd
@@ -43,7 +43,7 @@ export class LoginPage implements OnInit, OnDestroy {
     this.authService.openLogin();
   }
 
-  async skip(): Promise<any> {
+  async skip(): Promise<void> {
     this.slides.slideTo(this.slidesLength - 1);
   }
 }
