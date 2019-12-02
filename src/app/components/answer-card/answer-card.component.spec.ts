d119de8fe4844db3da49fbf81c6d33eb60b456a2
Norbert Bartko <mail@norbert-bartko.de>
2019-12-02 13:31:17 +0100

    Test answer card

--- src/app/components/answer-card/answer-card.component.spec.ts
+++ src/app/components/answer-card/answer-card.component.spec.ts
@@ -1,19 +1,43 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AnswerCardComponent } from './answer-card.component';
 import { CommentModule } from '../comment/comment.module';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { VotingModule } from '../voting/voting.module';
+
+import { IAnswer } from 'src/app/interfaces/answer';
 
 describe('AnswerCardComponent', () => {
   let component: AnswerCardComponent;
   let fixture: ComponentFixture<AnswerCardComponent>;
 
+  const testAnswer: IAnswer = {
+    answer_id: 1,
+    creation_date: 2,
+    question_id: 3,
+    score: 200,
+    is_accepted: true,
+    owner: null,
+    comments: [
+      {
+        post_id: 1,
+        owner: null,
+        body: `<h2> test comment </h2>`,
+        comment_id: 2,
+        creation_date: 3,
+        score: 4,
+        edited: false,
+      },
+    ],
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AnswerCardComponent ],
+      declarations: [ AnswerCardComponent, ],
       imports: [
         HttpClientTestingModule,
         IonicModule,
@@ -33,5 +57,26 @@ describe('AnswerCardComponent', () => {
 
   it('Should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should have at least one user card', () => {
+    component.answer = testAnswer;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('user-card')).length).toBeGreaterThanOrEqual(1);
+  });
+
+  it('Should have voting', () => {
+    component.answer = testAnswer;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('voting')).length).toEqual(1);
+  });
+
+  it('Should have a comment section', () => {
+    component.answer = testAnswer;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('comments')).length).toEqual(1);
   });
 });
