80e3638d2ab51337c04b4dd54adf8f9bec6f2754
Norbert Bartko <mail@norbert-bartko.de>
2019-12-03 15:17:26 +0100

    Implement loading animation while the user gets authenticated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { AuthGuard } from './guards/auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'menu', pathMatch: 'full' },
-  { path: 'menu', loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule), canActivate: [AuthGuard], canActivateChild: [AuthGuard] },
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule) },
+  { path: 'menu', loadChildren: () => import('./pages/menu/menu.module').then(m => m.MenuPageModule), canActivate: [AuthGuard] },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) },
 ];
 
 @NgModule({
