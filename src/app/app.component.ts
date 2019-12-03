ce34029452e2521f76831e70037cbfff5a882273
Norbert Bartko <mail@norbert-bartko.de>
2019-12-03 15:17:26 +0100

    Implement loading animation while the user gets authenticated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { GuardsCheckStart, Router, NavigationEnd, NavigationCancel, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  public loading: boolean;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router,
   ) {
     this.initializeApp();
+    this.loading = true;
   }
+
+  ngAfterViewInit() {
+    this.router.events
+      .subscribe((event: RouterEvent) => {
+        if (event instanceof GuardsCheckStart) {
+          this.loading = true;
+        } else if ( event instanceof NavigationEnd || event instanceof NavigationCancel) {
+          this.loading = false;
+        }
+      });
+}
 
   initializeApp() {
     this.platform.ready().then(() => {
