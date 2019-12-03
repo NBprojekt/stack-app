489387b38cb90b6bd9dfd3ceb2bf320d7bfec49b
Norbert Bartko <mail@norbert-bartko.de>
2019-12-03 17:42:59 +0100

    Change from can activate to can load to increase performance

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -1,17 +1,37 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import {
+  CanActivate,
+  CanActivateChild,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+  Route,
+  UrlSegment,
+  CanLoad,
+} from '@angular/router';
 
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, CanActivateChild {
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
 
   constructor(
     private authService: AuthService,
     private router: Router,
   ) { }
+
+  public async canLoad(route: Route, segments: UrlSegment[]): Promise<boolean> {
+    const isAuthenticated = await this.authService.isAuthenticated();
+
+    if (!isAuthenticated) {
+      this.router.navigate(['/login']);
+    }
+
+    return isAuthenticated;
+  }
 
   public async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean | UrlTree> {
     const isAuthenticated = await this.authService.isAuthenticated();
