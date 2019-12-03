b4a089d1c32094bf331ae43c4ac962f2785bead8
Norbert Bartko <mail@norbert-bartko.de>
2019-12-03 15:16:03 +0100

    Implement the token validation and make the isAuthenticated promise based

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,20 +1,21 @@
 import { Injectable, NgZone } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 import { InAppBrowser, InAppBrowserEvent, InAppBrowserObject } from '@ionic-native/in-app-browser/ngx';
 
 import { environment } from 'src/environments/environment';
 
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { ToastController } from '@ionic/angular';
+import { IResponse } from 'src/app/interfaces/response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private readonly oAuthUrl = `https://stackoverflow.com/oauth/dialog?client_id=${environment.oAuth.clientId}&redirect_uri=${environment.oAuth.redirectUrl}&scope=${environment.oAuth.scope}`;
-  private token;
+  private readonly apiUrl = environment.api.url + environment.api.version;
   private allowBrowserClose: boolean;
 
   constructor(
@@ -51,15 +52,23 @@ export class AuthService {
     });
   }
 
-  public isAuthenticated(): boolean {
-    // TODO: Implement validation token function
-    return !!this.token;
+  public async isAuthenticated(): Promise<boolean> {
+    return new Promise((resolve) => {
+      this.validateToken(this.getToken()).subscribe((response: IResponse) => {
+        resolve(response && response.items.length > 0);
+      });
+    });
   }
   public getToken(): string {
     return this.token;
   }
 
-  private validateToken() {}
+  public validateToken(token: string): Observable<IResponse> {
+    const headers = new HttpHeaders()
+      .set('Accept', '*/*');
+
+    return this.http.get<IResponse>(`${this.apiUrl}access-tokens/${token}`, {headers});
+  }
 
   private successfulLogin(url: string): void {
     if (environment.production) {
