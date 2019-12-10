9f3f07e75c02844785d1776c2561c1691e403256
Norbert Bartko <mail@norbert-bartko.de>
2019-12-10 16:14:12 +0100

    Update install script to work properly

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
-cd ..
-
 echo "> Installing dependencies"
 npm i
 
-echo "> Restoring project"
-ionic state restore
+echo "> Restoring resources"
+ionic cordova resources
 
+echo ""
 read -r -p "> Do you want to add the android  platform? [Y/n] " response
 case "$response" in
-    [nN])
-      exit
-      ;;
+    [nN]) ;;
     *)
       ionic cordova platform add android
       ;;
 esac
 
+echo ""
 read -r -p "> Do you want to add the ios platform? [Y/n] " response
 case "$response" in
-    [nN])
-      exit
-      ;;
+    [nN]) ;;
     *)
       ionic cordova platform add ios
       ;;
 esac
 
-echo "> Restoring resources"
-ionic resources
-
+echo ""
 echo "> Done."
