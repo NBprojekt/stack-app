a38dff4605ff322c0f78a851f04191b14529dffb
Norbert Bartko <mail@norbert-bartko.de>
2019-12-13 13:17:46 +0100

    Implement voting on questions

--- src/app/components/voting/voting.component.ts
+++ src/app/components/voting/voting.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input } from '@angular/core';
 
+import { AlertController } from '@ionic/angular';
+
+import { QuestionsService } from 'src/app/services/questions/questions.service';
+
+import { IResponse, IResponseError } from 'src/app/interfaces/response';
+
 @Component({
   selector: 'voting',
   templateUrl: './voting.component.html',
@@ -19,32 +25,104 @@ export class VotingComponent {
   @Input() showAccepted?: boolean;
   @Input() isAccepted?: boolean;
 
+  constructor(
+    private questionService: QuestionsService,
+    private alertController: AlertController,
+  ) {}
 
+  /**
+   * TODO: Refactor this shitty code
+   * It works tho :D
+   */
   public toggleUpvote(): void {
-
     if (this.upvoted) {
-      this.upvoted = false;
-      this.score--;
+      this.questionService
+        .upvoteQuestionUndo(this.questionId)
+        .subscribe(
+          () => {
+            this.score--;
+            this.upvoted = false;
+          },
+          (response: any) => this.errorHandler(response.error as IResponseError),
+        );
     } else {
-      this.upvoted = true;
-      this.downvoted ? this.score += 2 : this.score++;
-      this.downvoted = false;
+      if (this.downvoted) {
+        this.questionService
+          .downvoteQuestionUndo(this.questionId)
+          .subscribe(
+            () => {
+              this.questionService
+                .upvoteQuestion(this.questionId)
+                .subscribe(
+                  () => {
+                    this.score += 2;
+                    this.upvoted = true;
+                    this.downvoted = false;
+                  },
+                  (response: any) => this.errorHandler(response.error as IResponseError),
+                );
+            },
+            (response: any) => this.errorHandler(response.error as IResponseError),
+          );
+      } else {
+        this.questionService
+          .upvoteQuestion(this.questionId)
+          .subscribe(
+            () => {
+              this.score++;
+              this.upvoted = true;
+            },
+            (response: any) => this.errorHandler(response.error as IResponseError),
+          );
+      }
     }
   }
   public toggleDownvote(): void {
-
     if (this.downvoted) {
-      this.downvoted = false;
-      this.score++;
+      this.questionService
+        .downvoteQuestionUndo(this.questionId)
+        .subscribe(
+          () => {
+            this.score++;
+            this.downvoted = false;
+          },
+          (response: any) => this.errorHandler(response.error as IResponseError),
+        );
     } else {
-      this.downvoted = true;
-      this.upvoted ? this.score -= 2 : this.score--;
-      this.upvoted = false;
+      if (this.upvoted) {
+        this.questionService
+          .upvoteQuestionUndo(this.questionId)
+          .subscribe(
+            () => {
+              this.questionService
+                .downvoteQuestion(this.questionId)
+                .subscribe(
+                  () => {
+                    this.score -= 2;
+                    this.upvoted = false;
+                    this.downvoted = true;
+                  },
+                  (response: any) => this.errorHandler(response.error as IResponseError),
+                );
+            },
+            (response: any) => this.errorHandler(response.error as IResponseError),
+          );
+      } else {
+        this.questionService
+          .downvoteQuestion(this.questionId)
+          .subscribe(
+            () => {
+              this.score--;
+              this.downvoted = true;
+            },
+            (response: any) => this.errorHandler(response.error as IResponseError),
+          );
+      }
     }
   }
+  // End of Refactor this shitty code
 
   public toggleFavorite(): void {
-
     if (this.isFavorite) {
       this.isFavorite = false;
       this.countFavorites--;
@@ -56,5 +134,15 @@ export class VotingComponent {
 
   public toggleAccepted(): void {
     this.isAccepted = !this.isAccepted;
+  }
+
+  private async errorHandler(response: IResponseError): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Can\'t cast this vote',
+      message: response.error_message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
