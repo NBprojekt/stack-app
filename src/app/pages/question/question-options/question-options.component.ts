d2e2cf04b983a88d1174105bf5edb36fd78721f7
Norbert Bartko <mail@norbert-bartko.de>
2019-12-20 18:24:51 +0100

    Create question options component

---
+++ src/app/pages/question/question-options/question-options.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-options',
+  templateUrl: './question-options.component.html',
+  styleUrls: ['./question-options.component.scss'],
+})
+export class QuestionOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
