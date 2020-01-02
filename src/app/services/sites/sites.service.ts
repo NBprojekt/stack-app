1b6785448606fa95aa63dd4afba9442575239a98
Norbert Bartko <mail@norbert-bartko.de>
2020-01-02 18:52:14 +0100

    Add set, get and ready functions to site service

--- src/app/services/sites/sites.service.ts
+++ src/app/services/sites/sites.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
 
+import { Storage } from '@ionic/storage';
+
 @Injectable({
   providedIn: 'root'
 })
 export class SitesService {
+  private site: any;
 
-  constructor() { }
+  constructor(
+    private storage: Storage,
+  ) {}
+
+  public getCurrentSite(): any | Promise<any> {
+    return this.site ? this.site : this.storage.get('current_site');
+  }
+
+  public async setCurrentSite(site: any): Promise<void> {
+    return new Promise(async (resolve) => {
+      this.site = site;
+      await this.storage.set('current_site', site);
+      console.log(`New Site is now ${site.name}`)
+      resolve();
+    });
+
+  }
+
+  public async ready(): Promise<void> {
+    return new Promise(async (resolve) => {
+      this.site = await this.storage.get('current_site');
+      resolve();
+    });
+  }
 }
