c309c4997fdb3239b30a970a2551ad48278026d8
Norbert Bartko <mail@norbert-bartko.de>
2020-03-11 10:17:06 +0100

    Create me card component

---
+++ src/app/pages/menu/me-card/me-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'me-card',
+  templateUrl: './me-card.component.html',
+  styleUrls: ['./me-card.component.scss'],
+})
+export class MeCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
