8d8eee991413be1fc4e7442e396a37e74c2ce702
Norbert Bartko <mail@norbert-bartko.de>
2020-03-12 13:40:05 +0100

    Create sites page

---
+++ src/app/pages/sites/sites.page.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sites',
+  templateUrl: './sites.page.html',
+  styleUrls: ['./sites.page.scss'],
+})
+export class SitesPage {
+}
