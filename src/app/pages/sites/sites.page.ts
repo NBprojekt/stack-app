09a4afeb0f4c08248089cc66b81cca2f8b9185e3
Norbert Bartko <mail@norbert-bartko.de>
2020-03-13 17:52:00 +0100

    Remove debuglogs

--- src/app/pages/sites/sites.page.ts
+++ src/app/pages/sites/sites.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { SitesService } from 'src/app/services/sites/sites.service';
 import { UserService } from 'src/app/services/user/user.service';
 
-import { ISite } from 'src/app/interfaces/site';
+import { ISite, IMySite } from 'src/app/interfaces/site';
 import { IResponse } from 'src/app/interfaces/response';
 
 @Component({
@@ -27,18 +27,15 @@ export class SitesPage implements OnInit {
     this.sites = await this.siteService.getAllSites();
 
     this.userService.getMySites().subscribe((response: IResponse) => {
-      console.log(['SITES', this.sites, 'MY SITES', response.items]);
+      response.items.forEach((mySite: IMySite) => {
+        const matchingIndex = this.sites.findIndex(s => s.site_url.toLowerCase() === mySite.site_url.toLowerCase());
 
-      response.items.forEach((mySite: {site_name: string, reputation: number}) => {
-        const matchingIndex = this.sites.findIndex(s => s.name === mySite.site_name);
         if (matchingIndex >= 0) {
           this.sites[matchingIndex].reputation = mySite.reputation;
         }
       });
 
       this.sites.sort((a, b) => (b.reputation || 0) - (a.reputation || 0));
-
-      console.log(['SORTED SITES ARRAY', this.sites]);
     });
   }
 }
