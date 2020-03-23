e3cd2c8292f06fd56ad57eeb29bf9517f942d9d3
Norbert Bartko <bartko.norbert@gmail.com>
2020-03-23 05:10:03 +0100

    Show join site dialog and open browser to join community

--- src/app/pages/sites/sites.page.ts
+++ src/app/pages/sites/sites.page.ts
@@ -1,40 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { AlertController } from '@ionic/angular';
+import { InAppBrowser, InAppBrowserEvent, InAppBrowserObject } from '@ionic-native/in-app-browser/ngx';
 
 import { SitesService } from 'src/app/services/sites/sites.service';
 import { UserService } from 'src/app/services/user/user.service';
 
 import { ISite, IMySite } from 'src/app/interfaces/site';
 import { IResponse } from 'src/app/interfaces/response';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sites',
   templateUrl: './sites.page.html',
   styleUrls: ['./sites.page.scss'],
 })
-export class SitesPage implements OnInit {
+export class SitesPage implements OnInit, OnDestroy {
   public readonly maxItems = 20;
   public showItems: number;
   public searchString: string;
 
+  public loading: boolean;
   public sites: Array<ISite>;
   private _sites: Array<ISite>;
 
+  private browserSubscribtion$: Subscription;
+
   constructor(
     private siteService: SitesService,
+    private inAppBrowser: InAppBrowser,
     private userService: UserService,
+    private alertController: AlertController,
+    private router: Router,
   ) {}
 
   public ngOnInit(): void {
+    this.loading = true;
     this.showItems = this.maxItems;
     this.loadSites();
   }
 
-  public openSite(site: ISite): void {
-
+  public ngOnDestroy(): void {
+    this.browserSubscribtion$.unsubscribe();
   }
 
-  public joinSiteDialog(site: ISite): void {
-    
+  public async openSite(site: ISite): Promise<void> {
+    await this.siteService.setCurrentSite(site);
+    this.router.navigateByUrl('/menu/pages/tabs/home');
+  }
+
+  public async joinSiteDialog(site: ISite): Promise<void> {
+    const alert = await this.alertController.create({
+      header: `Join ${site.name}?`,
+      message: `Do you realy want to join ${site.name}?`,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        }, {
+          text: 'Okay',
+          handler: () => this.openJoinSite(site)
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  private openJoinSite(site): void {
+    const browser: InAppBrowserObject = this.inAppBrowser.create(
+      `${site.url}/users/join?returnurl=${site.url}/return`, 
+      '_blank', 'location=no,zoom=no,shouldPauseOnSuspend=yes,hidden=yes,beforeload=yes'
+    );
+
+    browser.insertCSS({ code: `
+      body: { padding: 0 }
+      header: { display: none; visibility: hidden }
+    ` });
+
+    this.browserSubscribtion$ = browser.on('loadstart').subscribe((event: InAppBrowserEvent) => {
+      const url = event.url.toLowerCase();
+
+      if (url.startsWith(`${site.url}/return`)) {
+        browser.close();
+
+        this.router.navigateByUrl('/menu/sites');
+
+        this.browserSubscribtion$.unsubscribe();
+      }
+    });
   }
 
   private async loadSites(): Promise<void> {
@@ -52,6 +107,8 @@ export class SitesPage implements OnInit {
 
       this.sites.sort((a, b) => (b.reputation || 0) - (a.reputation || 0));
       this._sites = this.sites;
+
+      this.loading = false; 
     });
   }
 
@@ -65,8 +122,8 @@ export class SitesPage implements OnInit {
     this.search();
   }
 
-  public hasMore(): boolean {
-    return this.sites && this.sites.length > this.showItems;
+  public hasMore(): number {
+    return this.sites ? this.sites.length - this.showItems : -1;
   }
   public showMore(): void {
     this.showItems += this.maxItems;
