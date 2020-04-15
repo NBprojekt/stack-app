09675096a19637a0a2c48ace299cd41cb5abed76
Norbert Bartko <bartko.norbert@gmail.com>
2020-04-15 17:35:55 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -25,9 +25,9 @@ function welcome () {
   printf "   | (___ | |_ __ _  ___| | __   __ _ _ __  _ __    \n"
   printf "    \___ \| __/ _\` |/ __| |/ /  / _\` | '_ \| '_ \ \n"
   printf "    ____) | || (_| | (__|   <  | (_| | |_) | |_) |  \n"
-  printf "   |_____/ \__\__,_|\___|_|\_\  \__,_| .__/| .__/   $version\n"
+  printf "   |_____/ \__\__,_|\___|_|\_\  \__,_| .__/| .__/   \n"
   printf "                                     | |   | |      \n"
-  printf "                                     |_|   |_|      \n\n"
+  printf "      v$version\t\t\t     |_|   |_|                 \n\n"
 
   printf "✨ Welcome to the stack app installer ✨ \n\n"
   printf "  • Install all cli's needed and dependencies \n"
@@ -38,7 +38,7 @@ function welcome () {
 }
 
 function install() {
-  printf "\n\n>\n> ✨ Starting installation ✨\n"
+  printf "\n\n\n> ✨ Starting installation ✨\n"
   printf "> Installing global cli's"
   npm i -g @angular/cli@^8.0.0 ionic@^5.0.0 cordova@^9.0.0 >/dev/null 2>&1 & spinner
 
@@ -47,26 +47,30 @@ function install() {
 }
 
 function configure() {
-  printf ">\n> ✨ Starting configuration ✨\n"
+  printf "\n> ✨ Starting configuration ✨\n"
   printf "> Restoring resources"
   ionic cordova resources  >/dev/null 2>&1 & spinner
 
-  printf ">\n> Do you want to add the android  platform? [Y/n]"
+  printf "> Do you want to add the android  platform? [Y/n]"
   read -r -p " " response
   case "$response" in
-      [nN]) ;;
+      [nN])
+        printf "> Skipped adding android platform\n"
+        ;;
       *)
         printf "> Adding android platform"
         ionic cordova platform add android >/dev/null 2>&1 & spinner
         ;;
   esac
 
-  printf ">\n> Do you want to add the ios platform? [Y/n]"
+  printf "\n> Do you want to add the ios platform? [Y/n]"
   read -r -p " " response
   case "$response" in
-      [nN]) ;;
+      [nN])
+        printf "> Skipped adding ios platform\n"
+        ;;
       *)
-        printf "\n> Adding ios platform"
+        printf "> Adding ios platform"
         ionic cordova platform add ios >/dev/null 2>&1 & spinner
         ;;
   esac
@@ -77,5 +81,4 @@ welcome
 install
 configure
 
-
-printf ">\n>\n> Done.\n"
+printf "\n\n> Done.\n"
