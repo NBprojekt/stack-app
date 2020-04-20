8f01016a7b485026219c7cdfbdcab29650ac149c
Norbert Bartko <bartko.norbert@gmail.com>
2020-04-20 16:53:13 +0200

    Add test template

--- src/app/pages/tabs/search/search.page.ts
+++ src/app/pages/tabs/search/search.page.ts
@@ -5,4 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
-export class SearchPage {}
+export class SearchPage {
+
+  public readonly maxItems = 20;
+  public showItems: number;
+  public searchString: string;
+
+
+  constructor() {}
+
+  public search(): void {
+    const searchString = this.searchString ? this.searchString.toLowerCase().trim() : '';
+
+    console.log(`Searching for ${searchString}`);
+  }
+
+  public resetFilter(): void {
+    this.searchString = '';
+    console.log(`Cleared Searchstring`);
+  }
+}
