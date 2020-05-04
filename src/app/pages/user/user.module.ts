3104c7dda889377d407f10560c95643809834e35
Norbert Bartko <bartko.norbert@gmail.com>
2020-05-04 17:31:51 +0200

    Install chart.js

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
-
 import { Routes, RouterModule } from '@angular/router';
+
+import { ChartsModule } from 'ng2-charts';
 
 import { UserPage } from '../user/user.page';
 import { CommonDirectivesModule } from 'src/app/directives/common-directives.module';
@@ -20,6 +21,7 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes),
     CommonDirectivesModule,
+    ChartsModule,
   ],
   declarations: [
     UserPage,
