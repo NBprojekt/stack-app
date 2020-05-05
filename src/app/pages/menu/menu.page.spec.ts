6e6d5e54950d367eae2caefc394495ff9a01a175
Norbert Bartko <bartko.norbert@gmail.com>
2020-05-05 18:52:20 +0200

    Fix up page tests

--- src/app/pages/menu/menu.page.spec.ts
+++ src/app/pages/menu/menu.page.spec.ts
@@ -1,7 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { IonicModule } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 import { MenuPage } from './menu.page';
 
@@ -11,8 +15,20 @@ describe('MenuPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        IonicModule,
+        RouterTestingModule,
+      ],
       declarations: [ MenuPage ],
+      providers: [
+        InAppBrowser,
+        {
+          provide: Storage, useValue: {
+            get: () => new Promise<any>((resolve, reject) => resolve('test')),
+          }
+        },
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
