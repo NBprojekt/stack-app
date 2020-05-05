12b46292db3f6a43b85658042cb8fa3b5ab7be3c
Norbert Bartko <bartko.norbert@gmail.com>
2020-05-05 18:52:04 +0200

    Fix up service tests

--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -1,9 +1,32 @@
-import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { IonicModule } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        IonicModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        InAppBrowser,
+        {
+          provide: Storage, useValue: {
+            get: () => new Promise<any>((resolve, reject) => resolve('test')),
+          }
+        },
+      ]
+    });
+  }));
 
   it('should be created', () => {
     const service: UserService = TestBed.get(UserService);
