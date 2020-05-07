e479990e328248eff7d9187062a450fc4172c45a
Norbert Bartko <bartko.norbert@gmail.com>
2020-05-07 13:26:29 +0200

    User const for default icon size

--- src/app/icons/browser/browser.component.ts
+++ src/app/icons/browser/browser.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 
+import { DEFAULT_ICON_SIZE } from '../icons.module';
+
 @Component({
   selector: 'icon-browser',
   templateUrl: './browser.component.html',
 })
 export class BrowserComponent {
-  @Input() width = '200px';
-  @Input() height = '200px';
+  @Input() width = DEFAULT_ICON_SIZE;
+  @Input() height = DEFAULT_ICON_SIZE;
 
   @Input() primary = 'var(--ion-color-secondary)';
   @Input() secondary = 'var(--ion-color-secondary-tint)';
