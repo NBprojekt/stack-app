73e67e03a1623b46218f39a7f3ab62a0600a802a
Norbert Bartko <bartko.norbert@gmail.com>
2020-05-07 13:06:17 +0200

    Create browser icon

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MapMarkerComponent } from './map-marker/map-marker.component';
+import { BrowserComponent } from './browser/browser.component';
 
 @NgModule({
   declarations: [
     MapMarkerComponent,
+    BrowserComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
     MapMarkerComponent,
+    BrowserComponent,
   ],
 })
 export class IconsModule { }
