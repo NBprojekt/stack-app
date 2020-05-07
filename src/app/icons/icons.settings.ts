467b3dccfcd05c12a96d8fe3e7602bf03bebbe3b
Norbert Bartko <bartko.norbert@gmail.com>
2020-05-07 13:30:42 +0200

    Create icon settings to avoid circular dependencies

---
+++ src/app/icons/icons.settings.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_ICON_SIZE = '25px';
+
+export const DEFAULT_ICON_COLOR_PRIMARY = 'var(--ion-color-secondary)';
+export const DEFAULT_ICON_COLOR_SECONDARY = 'var(--ion-color-secondary-tint)';
