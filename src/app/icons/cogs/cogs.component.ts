9acf2510d09de3f11c2436367aa8f889106dead3
Norbert Bartko <bartko.norbert@gmail.com>
2020-05-07 17:46:07 +0200

    Create cogs icon

---
+++ src/app/icons/cogs/cogs.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+import { DEFAULT_ICON_SIZE, DEFAULT_ICON_COLOR_PRIMARY, DEFAULT_ICON_COLOR_SECONDARY } from '../icons.settings';
+
+@Component({
+  selector: 'icon-cogs',
+  templateUrl: './cogs.component.html',
+})
+export class CogsComponent {
+  @Input() width = DEFAULT_ICON_SIZE;
+  @Input() height = DEFAULT_ICON_SIZE;
+
+  @Input() primary = DEFAULT_ICON_COLOR_PRIMARY;
+  @Input() secondary = DEFAULT_ICON_COLOR_SECONDARY;
+}
