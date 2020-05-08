7d43bb31692c2565110e9762d36b3221886ec0dd
Norbert Bartko <bartko.norbert@gmail.com>
2020-05-08 18:28:27 +0200

    Update tabs.page.spec.ts

--- src/app/pages/tabs/tabs.page.spec.ts
+++ src/app/pages/tabs/tabs.page.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { TabsPage } from './tabs.page';
 
@@ -9,6 +10,9 @@ describe('TabsPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ 
+        RouterTestingModule,
+      ],
       declarations: [ TabsPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
