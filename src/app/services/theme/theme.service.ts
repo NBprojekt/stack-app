f47fcb397dc5a6949beaab18c40560ca0814982e
Norbert Bartko <bartko.norbert@gmail.com>
2020-07-08 15:29:50 +0200

    Add themeservice mode logic

--- src/app/services/theme/theme.service.ts
+++ src/app/services/theme/theme.service.ts
@@ -5,5 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class ThemeService {
 
-  constructor() { }
+  constructor() {
+    document.body.classList.add(this.getMode());
+  }
+
+  public setMode(mode: 'dark' | 'light'): void {
+    document.cookie = `mode=${mode}`;
+  }
+  public getMode(): string {
+    const cookie = document.cookie.split(';').map(x => x.split('=')).find(x => x[0].trim() === 'mode');
+    return cookie ? cookie[1] : 'light';
+  }
 }
