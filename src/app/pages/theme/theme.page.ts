c9336aa648c4a8ac6dcca6c42f5669c11c6c31c1
Norbert Bartko <bartko.norbert@gmail.com>
2020-07-09 12:34:14 +0200

    Add theme selection on theme page

--- src/app/pages/theme/theme.page.ts
+++ src/app/pages/theme/theme.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/services/theme/theme.service';
 
 @Component({
   selector: 'app-theme',
@@ -6,4 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./theme.page.scss'],
 })
 export class ThemePage {
+  public mode: 'light' | 'dim' | 'lights-out';
+
+  constructor (
+    private themeSerice: ThemeService,
+  ) {
+    this.mode = themeSerice.getMode();
+  }
+
+  public changeTheme(): void {
+    this.themeSerice.setMode(this.mode)
+  }
 }
