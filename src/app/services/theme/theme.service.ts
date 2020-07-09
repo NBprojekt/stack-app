d4ccc5d6997e3fa605ed44c88b3b558f8cf01217
Norbert Bartko <bartko.norbert@gmail.com>
2020-07-09 18:46:34 +0200

    Implement themes and enums

--- src/app/services/theme/theme.service.ts
+++ src/app/services/theme/theme.service.ts
@@ -21,21 +21,36 @@ export class ThemeService {
 
   constructor() {
     this.setMode(this.getMode());
+    this.setTheme(this.getTheme());
   }
 
-  public setMode(mode: 'light' | 'dim' | 'lights-out'): void {
+  // Modes
+  public setMode(mode: Modes | string): void {
     document.cookie = `mode=${mode}`;
 
     // Remove old modes
-    document.body.classList.remove('light');
-    document.body.classList.remove('dim');
-    document.body.classList.remove('lights-out');
+    Object.keys(Modes).map(key => document.body.classList.remove(Modes[key]));
 
     // Add new mode
     document.body.classList.add(mode);
   }
-  public getMode(): 'light' | 'dim' | 'lights-out' {
+  public getMode(): string {
     const cookie = document.cookie.split(';').map(x => x.split('=')).find(x => x[0].trim() === 'mode');
-    return cookie ? cookie[1] as 'light' | 'dim' | 'lights-out' : 'light';
+    return cookie ? cookie[1] : 'light';
+  }
+
+  // Themes
+  public setTheme(theme: Themes | string): void {
+    document.cookie = `theme=${theme}`;
+
+    // Remove old themes
+    Object.keys(Themes).map(key => document.body.classList.remove(Themes[key]));
+
+    // Add new theme
+    document.body.classList.add(theme);
+  }
+  public getTheme(): string {
+    const cookie = document.cookie.split(';').map(x => x.split('=')).find(x => x[0].trim() === 'theme');
+    return cookie ? cookie[1] : Themes.DEFAULT;
   }
 }
