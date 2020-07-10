ee377dbfb15d512384ecb137675fd9dbafc8c0a1
Norbert Bartko <bartko.norbert@gmail.com>
2020-07-10 18:02:16 +0200

    Fix up tests

--- src/app/pages/theme/theme.page.spec.ts
+++ src/app/pages/theme/theme.page.spec.ts
@@ -2,6 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ThemePage } from './theme.page';
+import { CommonPipesModule } from 'src/app/pipes/common-pipes.module';
 
 describe('ThemePage', () => {
   let component: ThemePage;
@@ -9,6 +10,9 @@ describe('ThemePage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        CommonPipesModule,
+      ],
       declarations: [ ThemePage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
