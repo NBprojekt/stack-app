68abdcb9904c2072ac27c861042aa52805cb7bcd
Norbert Bartko <bartko.norbert@gmail.com>
2020-07-10 17:01:38 +0200

    Create basic search page

--- src/app/pages/tabs/search/search.page.ts
+++ src/app/pages/tabs/search/search.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SearchService } from 'src/app/services/search/search.service';
+import { IQuestion } from 'src/app/interfaces/question';
+import { IRequestOptions } from 'src/app/interfaces/request-options';
 
 @Component({
   selector: 'app-search',
@@ -11,13 +14,36 @@ export class SearchPage {
   public showItems: number;
   public searchString: string;
 
+  public loading: boolean;
 
-  constructor() {}
+  public result: Array<IQuestion>;
+
+  private options: IRequestOptions;
+
+  constructor(
+    private searchService: SearchService,
+  ) {}
 
   public search(): void {
     const searchString = this.searchString ? this.searchString.toLowerCase().trim() : '';
 
     console.log(`Searching for ${searchString}`);
+
+    if(!searchString) {
+      this.result = null;
+      return;
+    }
+
+    this.loading = true;
+
+    this.searchService.searchAdvanced(searchString, this.options).subscribe(response => {
+      this.result = response.items;
+    });
+  }
+
+
+  public formatUrl(url: string): string {
+    return url.replace(/[^\w\s]/gi, '').split(' ').join('-');
   }
 
   public resetFilter(): void {
