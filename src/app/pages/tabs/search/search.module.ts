acb254fb06bfaa0181bab5ad8087a417507fd495
Norbert Bartko <bartko.norbert@gmail.com>
2020-07-10 17:01:38 +0200

    Create basic search page

--- src/app/pages/tabs/search/search.module.ts
+++ src/app/pages/tabs/search/search.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { SearchPage } from './search.page';
 import { IconsModule } from 'src/app/icons/icons.module';
+import { CommonPipesModule } from 'src/app/pipes/common-pipes.module';
 
 const routes: Routes = [
   {
@@ -22,6 +23,7 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes),
     IconsModule,
+    CommonPipesModule,
   ],
   declarations: [SearchPage]
 })
