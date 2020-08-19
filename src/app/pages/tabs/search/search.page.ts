c65481c38868f5f7e60cc3c35f24573d42a13bba
Norbert Bartko <bartko.norbert@gmail.com>
2020-08-19 18:13:27 +0200

    Add infinite scroll and fix loading issues

--- src/app/pages/tabs/search/search.page.ts
+++ src/app/pages/tabs/search/search.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ModalController } from '@ionic/angular';
 
@@ -6,23 +6,23 @@ import { SearchService } from 'src/app/services/search/search.service';
 import { IQuestion } from 'src/app/interfaces/question';
 import { IRequestOptions } from 'src/app/interfaces/request-options';
 import { SearchHelpComponent } from './search-help/search-help.component';
+import { IResponse } from 'src/app/interfaces/response';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
-export class SearchPage {
-
+export class SearchPage implements OnInit {
   public readonly maxItems = 20;
-  public showItems: number;
   public searchString: string;
   private _searchString: string;
 
   public loading: boolean;
 
-  public result: Array<IQuestion> = null;
+  public result: Array<IQuestion>;
   public total: number;
+  public hasMore: boolean;
 
   private options: IRequestOptions;
 
@@ -31,6 +31,12 @@ export class SearchPage {
     private modalController: ModalController,
   ) {}
 
+  public ngOnInit(): void {
+    this.result = null;
+    this.options = {};
+    this.hasMore = false;
+  }
+
   public search(): void {
     const searchString = this.searchString ? this.searchString.toLowerCase().trim() : '';
 
@@ -38,11 +44,13 @@ export class SearchPage {
       return;
     }
     this._searchString = searchString;
+    this.options = {page: 0, pagesize: 10};
     this.loading = true;
 
-    this.searchService.searchAdvanced(searchString, this.options).subscribe(response => {
+    this.searchService.searchAdvanced(searchString, this.options).subscribe((response: IResponse) => {
       this.result = response.items;
       this.total = response.total;
+      this.hasMore = response.has_more;
       this.loading = false;
     });
   }
@@ -67,5 +75,20 @@ export class SearchPage {
       component: SearchHelpComponent,
     });
     return await modal.present();
+  }
+
+
+  public loadMore(event?: any): void {
+    this.options.page++;
+
+    this.searchService.searchAdvanced(this._searchString, this.options).subscribe((response: IResponse) => {
+      this.result = this.result.concat(response.items as Array<IQuestion>);
+      this.hasMore = response.has_more;
+      this.total = response.total;
+
+      if (event) {
+        event.target.complete();
+      }
+    });
   }
 }
