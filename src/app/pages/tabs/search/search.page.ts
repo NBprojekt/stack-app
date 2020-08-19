1de25c3a018da3b837ee26a68fe80a54821ac25e
Norbert Bartko <bartko.norbert@gmail.com>
2020-08-19 11:25:57 +0200

    Fix up search loginc and style

--- src/app/pages/tabs/search/search.page.ts
+++ src/app/pages/tabs/search/search.page.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+
+import { ModalController } from '@ionic/angular';
+
 import { SearchService } from 'src/app/services/search/search.service';
 import { IQuestion } from 'src/app/interfaces/question';
 import { IRequestOptions } from 'src/app/interfaces/request-options';
+import { SearchHelpComponent } from './search-help/search-help.component';
 
 @Component({
   selector: 'app-search',
@@ -13,47 +17,56 @@ export class SearchPage {
   public readonly maxItems = 20;
   public showItems: number;
   public searchString: string;
+  private _searchString: string;
 
   public loading: boolean;
 
-  public result: Array<IQuestion>;
+  public result: Array<IQuestion> = null;
   public total: number;
 
   private options: IRequestOptions;
 
   constructor(
     private searchService: SearchService,
+    private modalController: ModalController,
   ) {}
 
   public search(): void {
     const searchString = this.searchString ? this.searchString.toLowerCase().trim() : '';
 
-    console.log(`Searching for ${searchString}`);
-
-    if(!searchString || searchString.length === 0) {
-      this.result = null;
+    if(!searchString || searchString.length === 0 || searchString === this._searchString) {
       return;
     }
+    this._searchString = searchString;
+    this.loading = true;
 
     this.searchService.searchAdvanced(searchString, this.options).subscribe(response => {
-      console.log('Search result', response)
       this.result = response.items;
       this.total = response.total;
       this.loading = false;
     });
   }
 
-  public loadingOnTyping(event: any): void {
-    this.loading = !!event.target.value && event.target.value.length > 0;
+  public searchFor(s: string): void {
+    this.searchString = s;
+    this.search();
   }
 
+  public resetFilter(): void {
+    this.searchString = '';
+    this.result = null;
+    this.total = null;
+    console.log(`Cleared Searchstring`);
+  }
 
   public formatUrl(url: string): string {
     return url.replace(/[^\w\s]/gi, '').split(' ').join('-');
   }
 
-  public resetFilter(): void {
-    this.searchString = '';
-    console.log(`Cleared Searchstring`);
+  public async openSearchHelp(): Promise<void> {
+    const modal = await this.modalController.create({
+      component: SearchHelpComponent,
+    });
+    return await modal.present();
   }
 }
