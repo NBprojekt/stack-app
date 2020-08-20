8b6aca44128bbde68520759b6b3932b59b1a63d1
Norbert Bartko <bartko.norbert@gmail.com>
2020-08-20 08:13:07 +0000

    Move mode and theme enum in its own file

--- src/app/services/theme/theme.service.ts
+++ src/app/services/theme/theme.service.ts
@@ -1,23 +1,13 @@
 import { Injectable } from '@angular/core';
 
-export enum Modes {
-  DEFAULT = 'light',
-  LIGHT = 'light',
-  DIM = 'dim',
-  LOGHTS_OUT = 'lights-out'
-}
-
-export enum Themes {
-  DEFAULT = 'theme-orange-blue',
-  ORANGE_BLUE = 'theme-orange-blue',
-  PURPLE_GREEN = 'theme-purple-green',
-  BLUE_PINK = 'theme-blue-pink',
-}
+import { Mode } from 'src/app/enums/mode.enum';
+import { Theme } from 'src/app/enums/theme.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
+  // TODO: Store mode and theme information using Storage 
 
   constructor() {
     this.setMode(this.getMode());
@@ -25,32 +15,32 @@ export class ThemeService {
   }
 
   // Modes
-  public setMode(mode: Modes | string): void {
+  public setMode(mode: Mode | string): void {
     document.cookie = `mode=${mode}; path=/`;
 
     // Remove old modes
-    Object.keys(Modes).map(key => document.body.classList.remove(Modes[key]));
+    Object.keys(Mode).map(key => document.body.classList.remove(Mode[key]));
 
     // Add new mode
     document.body.classList.add(mode);
   }
   public getMode(): string {
     const cookie = document.cookie.split(';').map(x => x.split('=')).find(x => x[0].trim() === 'mode');
-    return cookie ? cookie[1] : 'light';
+    return cookie ? cookie[1] : Mode.DEFAULT;
   }
 
   // Themes
-  public setTheme(theme: Themes | string): void {
+  public setTheme(theme: Theme | string): void {
     document.cookie = `theme=${theme}; path=/`;
 
     // Remove old themes
-    Object.keys(Themes).map(key => document.body.classList.remove(Themes[key]));
+    Object.keys(Theme).map(key => document.body.classList.remove(Theme[key]));
 
     // Add new theme
     document.body.classList.add(theme);
   }
   public getTheme(): string {
     const cookie = document.cookie.split(';').map(x => x.split('=')).find(x => x[0].trim() === 'theme');
-    return cookie ? cookie[1] : Themes.DEFAULT;
+    return cookie ? cookie[1] : Theme.DEFAULT;
   }
 }
