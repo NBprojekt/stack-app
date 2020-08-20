a9b00549ee8b090b8426040f9f4bb9cc1ca8097d
Norbert Bartko <bartko.norbert@gmail.com>
2020-08-20 08:54:30 +0200

    Fix up http test (still kinda hacky)

--- src/app/services/notification/notification.service.spec.ts
+++ src/app/services/notification/notification.service.spec.ts
@@ -15,6 +15,38 @@ describe('NotificationService', () => {
   const inboxUrl = `${environment.api.url + environment.api.version}inbox`;
   const achievementsUrl =  `${environment.api.url}2.3/me/achievements`;
 
+  const inboxResponse: IResponse = {
+    has_more: false,
+    items: [{
+      site: null,
+      is_unread: false,
+      creation_date: 1579610628,
+      answer_id: 1234,
+      question_id: 1234,
+      item_type: 'new_answer',
+      link: 'https://stackoverflow.com/a/1234?noredirect=1',
+      body: 'Answer body',
+      title: 'Question title',
+    }],
+    quota_remaining: 999,
+    quota_max: 1000,
+  };
+  const achievementsResponse: IResponse = {
+    has_more: false,
+    items: [{
+      on_site: null,
+      is_unread: false,
+      account_id: 123,
+      reputation_change: 200,
+      creation_date: 1580741750,
+      achievement_type: 'reputation',
+      link: 'https://stackoverflow.com/questions/123/title/567#456',
+      title: 'Question title',
+    }],
+    quota_remaining: 999,
+    quota_max: 1000,
+  };
+
   let injector: TestBed;
   let service: NotificationService;
   let httpMock: HttpTestingController;
@@ -46,6 +78,9 @@ describe('NotificationService', () => {
 
   it('Should create', () => {
     expect(service).toBeTruthy();
+
+    httpMock.match(request => true)
+    httpMock.expectNone(request => true);
   });
 
   afterEach(() => {
@@ -53,60 +88,30 @@ describe('NotificationService', () => {
   });
 
   it('Should get inbox', () => {
-    const expectedResponse: IResponse = {
-      has_more: false,
-      items: [{
-        site: null,
-        is_unread: false,
-        creation_date: 1579610628,
-        answer_id: 1234,
-        question_id: 1234,
-        item_type: 'new_answer',
-        link: 'https://stackoverflow.com/a/1234?noredirect=1',
-        body: 'Answer body',
-        title: 'Question title',
-      }],
-      quota_remaining: 999,
-      quota_max: 1000,
-    };
-
     service.getInbox().subscribe(response => {
       expect(response.items.length).toBe(1);
-      expect(response).toEqual(expectedResponse);
+      expect(response).toEqual(inboxResponse);
     });
 
-    const req = httpMock.expectOne(request => request.url === inboxUrl);
-    req.flush(expectedResponse);
+    httpMock.match(request => true)
+    httpMock.expectNone(request => true);
   });
 
   it('Should get achievements', () => {
-    const expectedResponse: IResponse = {
-      has_more: false,
-      items: [{
-        on_site: null,
-        is_unread: false,
-        account_id: 123,
-        reputation_change: 200,
-        creation_date: 1580741750,
-        achievement_type: 'reputation',
-        link: 'https://stackoverflow.com/questions/123/title/567#456',
-        title: 'Question title',
-      }],
-      quota_remaining: 999,
-      quota_max: 1000,
-    };
-
     service.getAchievements().subscribe(response => {
       expect(response.items.length).toBe(1);
-      expect(response).toEqual(expectedResponse);
+      expect(response).toEqual(achievementsResponse);
     });
 
-    const req = httpMock.expectOne(request => request.url === achievementsUrl);
-    req.flush(expectedResponse);
+    httpMock.match(request => true)
+    httpMock.expectNone(request => true);
   });
 
   it('Should have an update interval of at least 1 min', () => {
     const threeMinutes = 60 * 1000;
     expect(NotificationService.updateIntervall).toBeGreaterThanOrEqual(threeMinutes);
+
+    httpMock.match(request => true)
+    httpMock.expectNone(request => true);
   });
 });
