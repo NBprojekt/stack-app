ddfecee408bba49379b6ed56862b9b7e225ab0ef
Norbert Bartko <bartko.norbert@gmail.com>
2020-08-20 08:13:07 +0000

    Move mode and theme enum in its own file

---
+++ src/app/enums/mode.enum.ts
@@ -0,0 +1,7 @@
+
+export enum Mode {
+  DEFAULT = 'light',
+  LIGHT = 'light',
+  DIM = 'dim',
+  LOGHTS_OUT = 'lights-out'
+}
---
+++ src/app/enums/theme.enum.ts
@@ -0,0 +1,7 @@
+export enum Theme {
+  DEFAULT = 'theme-orange-blue',
+  ORANGE_BLUE = 'theme-orange-blue',
+  PURPLE_GREEN = 'theme-purple-green',
+  BLUE_PINK = 'theme-blue-pink',
+}
+  
